16a3fed661bd33ea00975fc3aaa9ea6b72f30322
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2022-12-01 21:29:32 +0100

    add FilmCard

---
+++ src/components/FilmCard.jsx
@@ -0,0 +1,16 @@
+import Star from './elements/star.png'
+const FilmCard= (props)=>{
+
+    return(
+        <div style={{flexDirection:'column',display:"flex",marginTop:"2%",borderWidth:2,borderStyle:"solid"}}>
+            <img src={props.image} alt="image" width="239" height="240" />
+            <p><strong>{props.title}</strong></p>
+            <a>{props.opis}</a>
+            <div style={{flexDirection:'row'}}>
+            <img src={Star} alt="gwaizda" width="35" height="35"/>
+            {props.rate}
+            </div>
+        </div>
+    );
+};
+export default FilmCard;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,94 @@
 import Logo from "./elements/Logo";
 import Search from "./elements/Search";
 import Button from "./elements/Button";
+import FilmCard from "./FilmCard";
 const HomePage= ()=>{
 
     return(
-        <><div style={{display: "flex",margin:1,alignItems:"center",alignContent:'space-between'}}>
-            
-            <Logo></Logo>
-            <Search ></Search>
-            <Button></Button>
-           
-        </div></>
+        <>
+        <div style={{backgroundColor:"#F0FFF0"}}>
+            <div style={{display: "flex",margin:1,alignItems:"center",alignContent:'space-between'}}>
+                <Logo></Logo>
+                <Search ></Search>
+                <Button></Button>
+
+            </div>
+            <div style={{flexDirection:"row",display:"flex",alignItems:"center",justifyContent:"space-evenly",marginTop:"5%",flexWrap:"wrap",alignContent:"space-between"}}>
+                <FilmCard
+                    image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/239px-American_Beaver.jpg"}
+                    title={"Przygody Bobra Andrzeja"}
+                    opis={"Andrzej to dobry bóbr"}
+                    rate={"9/10"}
+                />
+                <FilmCard
+                    image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/239px-American_Beaver.jpg"}
+                    title={"Przygody Bobra Andrzeja"}
+                    opis={"Andrzej to dobry bóbr"}
+                    rate={"9/10"}
+                />
+                <FilmCard
+                    image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/239px-American_Beaver.jpg"}
+                    title={"Przygody Bobra Andrzeja"}
+                    opis={"Andrzej to dobry bóbr"}
+                    rate={"9/10"}
+                />
+                <FilmCard
+                    image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/239px-American_Beaver.jpg"}
+                    title={"Przygody Bobra Andrzeja"}
+                    opis={"Andrzej to dobry bóbr"}
+                    rate={"9/10"}
+                />
+                <FilmCard
+                    image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/239px-American_Beaver.jpg"}
+                    title={"Przygody Bobra Andrzeja"}
+                    opis={"Andrzej to dobry bóbr"}
+                    rate={"9/10"}
+                />
+                <FilmCard
+                    image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/239px-American_Beaver.jpg"}
+                    title={"Przygody Bobra Andrzeja"}
+                    opis={"Andrzej to dobry bóbr"}
+                    rate={"9/10"}
+                />
+                <FilmCard
+                    image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/239px-American_Beaver.jpg"}
+                    title={"Przygody Bobra Andrzeja"}
+                    opis={"Andrzej to dobry bóbr"}
+                    rate={"9/10"}
+                />
+                <FilmCard
+                    image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/239px-American_Beaver.jpg"}
+                    title={"Przygody Bobra Andrzeja"}
+                    opis={"Andrzej to dobry bóbr"}
+                    rate={"9/10"}
+                />
+                <FilmCard
+                    image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/239px-American_Beaver.jpg"}
+                    title={"Przygody Bobra Andrzeja"}
+                    opis={"Andrzej to dobry bóbr"}
+                    rate={"9/10"}
+                />
+                <FilmCard
+                    image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/239px-American_Beaver.jpg"}
+                    title={"Przygody Bobra Andrzeja"}
+                    opis={"Andrzej to dobry bóbr"}
+                    rate={"9/10"}
+                />
+                <FilmCard
+                    image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/239px-American_Beaver.jpg"}
+                    title={"Przygody Bobra Andrzeja"}
+                    opis={"Andrzej to dobry bóbr"}
+                    rate={"9/10"}
+                />
+                <FilmCard
+                    image={"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/American_Beaver.jpg/239px-American_Beaver.jpg"}
+                    title={"Przygody Bobra Andrzeja"}
+                    opis={"Andrzej to dobry bóbr"}
+                    rate={"9/10"}
+                />
+            </div>
+        </div>
+        </>
     );
 };
 export default HomePage;
