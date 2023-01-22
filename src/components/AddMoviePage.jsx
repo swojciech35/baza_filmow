349dd7de1b51118ac673e173f3abbe140573d444
swojciech35 <104626609+swojciech35@users.noreply.github.com>
2023-01-22 14:59:46 +0100

    add movie

--- src/components/AddMoviePage.jsx
+++ src/components/AddMoviePage.jsx
@@ -1,6 +1,23 @@
 import Logo from "./elements/Logo";
 import "../App.css";
+import { useState } from "react";
+import axios from "axios";
 const AddMoviePage = () => {
+  const [title, setTitle] = useState(null);
+  const [image, setImage] = useState(null);
+  const [content, setContent] = useState(null);
+
+  const addMovie = (title, image, content) => {
+    axios({
+      method: "post",
+      url: "https://at.usermd.net/api/movies",
+      data: {
+        title: title,
+        image: image,
+        content: content,
+      },
+    });
+  };
   return (
     <>
       <div className="loginPage">
@@ -15,22 +32,42 @@ const AddMoviePage = () => {
               type="text"
               class="form-control"
               id="formGroupExampleInput"
-              placeholder="Login"
+              placeholder="Tytuł"
+              onChange={(e) => setTitle(e.target.value)}
             />
           </div>
           <div class="form-group" style={{ paddingTop: 30 }}>
             <label for="formGroupExampleInput2">
-              <h4>Opis:</h4>{" "}
+              <h4>Zdjęcie:</h4>{" "}
             </label>
             <input
               type="text"
               class="form-control"
               id="formGroupExampleInput2"
-              placeholder="Hasło"
+              placeholder="link do zdjęcia"
+              onChange={(e) => setImage(e.target.value)}
+            />
+          </div>
+          <div class="form-group" style={{ paddingTop: 30 }}>
+            <label for="formGroupExampleInput3">
+              <h4>Opis:</h4>{" "}
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="formGroupExampleInput3"
+              placeholder="Opis"
+              onChange={(e) => setContent(e.target.value)}
             />
           </div>
           <div className="login-buttonLogin">
-            <button type="button" class="btn btn-secondary">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              onClick={() => {
+                addMovie(title, image, content);
+              }}
+            >
               Dodaj film
             </button>
           </div>
